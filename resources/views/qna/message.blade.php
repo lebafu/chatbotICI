4c6838c579cbbe0d158f787d487a633b38be8ee2
lebafu <50503857+lebafu@users.noreply.github.com>
2021-06-22 19:49:13 -0300

    Modificando Message de Actualizacion Qna y Mensaje Inicial de Bienvenida
    Modificando Message de Actualizacion Qna y Mensaje Inicial de Bienvenida

--- resources/views/qna/message.blade.php
+++ resources/views/qna/message.blade.php
@@ -1,16 +1,49 @@
-@extends('intents.layouts')
+<x-app-layout>
+         <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="http://localhost:3000/assets/modules/channel-web/inject.js"></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+
+    <!-- Styles -->
+      <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Actualización Exitosa') }}
+        </h2>
+    </x-slot>
+
+   
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
  
 @section('content')
 El archivo Modificado fue 
-Pregunta:{{$question->pregunta}}  //
-Respuesta:{{$answer->nombre}} :
+<p>Pregunta:{{$question->pregunta}}<p>
+<p>Respuesta:{{$answer->nombre}} </p>
+
+
+<!--<button class="btn btn-primary" href="/dashboard">Ir Dashboard</button>-->
+</x-app-layout>
 
 
 
 
 
-DATOS NUEVOS:
-{{$datosnuevos}}
 
-@endsection
-© 2020 GitHub, Inc.
