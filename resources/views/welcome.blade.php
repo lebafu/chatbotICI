3dbd054fbcdc18de0b4607a11c2f41dd5569f0a9
lebafu <50503857+lebafu@users.noreply.github.com>
2021-08-21 03:10:11 -0400

    Iniciando Sesión en Welcome
    Iniciando Sesión en Welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -56,22 +56,14 @@ body {
                 <!--<div class="col-1"></div>-->
     <div id="login" class="col-2">
         <div class="login">
-            <form id="form1" @submit.prevent="submit()">
+            <form method="POST" action="{{ route('login') }}">    
                 <div class="center-align">
-                   @auth
-                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
-                    @else
-
-
-
-
-
-
-
-
-<x-guest-layout>
+    
+           
+ <x-guest-layout>
     <x-jet-authentication-card>
-        <x-slot name="logo">
+
+        <x-slot name="logo" >
             <img src="images/logo.png">
         </x-slot>
         <x-jet-validation-errors class="mb-4" />
@@ -80,42 +72,44 @@ body {
                 {{ session('status') }}
             </div>
         @endif
-        <form method="POST" action="{{ route('login') }}">
             @csrf
             <div>
                 <x-jet-label for="email" value="{{ __('Email') }}" />
                 <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
             </div>
+
             <div class="mt-4">
                 <x-jet-label for="password" value="{{ __('Password') }}" />
                 <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
             </div>
+
             <div class="block mt-4">
                 <label for="remember_me" class="flex items-center">
                     <x-jet-checkbox id="remember_me" name="remember" />
                     <span class="ml-2 text-sm text-gray-600">{{ __('Recuérdame') }}</span>
                 </label>
             </div>
+
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                         {{ __('¿Olvidaste tu contraseña?') }}
                     </a>
                 @endif
+
                 <x-jet-button class="ml-4">
                     {{ __('Iniciar Sesión') }}
                 </x-jet-button>
             </div>
-        </form>
-    </x-jet-authentication-card>
+        
+       </x-jet-authentication-card>
 </x-guest-layout>
+</form>
 
 
 
-
-                    @endauth
                 </div>
-            </form>
+            <!--</form>-->
         </div>
     
     </div>
@@ -147,7 +141,37 @@ body {
                         </button>
                     </div>
             </div>
+            <script>
 
+                window.botpressWebChat.init({
+                   host:'http://localhost:3000',
+                   botId:'welcome',
+                   botName:'welcome',
+                   botAvatarUrl:null,
+                   botConvoDescription: 'El chatbot de ICI-UCM',
+                   backgroundColor: '#000000',
+                   textonColorbackground: '#000000',
+                   foregroundColor: '#00000',
+                   textColorOnForeground: '#000000',
+                   showConversation: false,
+                   enableTranscriptDowload: false,
+                   enableArrowNavigation: false,
+                   userId:null,
+                   enableReset:false,
+                   hideWidget:false,
+
+                })
+
+                window.addEventListener('message',function(event){
+                    if(event.data.name==='webchatOpened'){
+                        window.botpressWebChat.sendEvent({
+                            type:'proactive-trigger',
+                            channel:'web',
+                            payload: {text: 'fake message'}
+                        })
+                    }
+                })
+            </script>
 
     </body>
 </html>
