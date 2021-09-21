f0536014f511f9f0364cc2d46c55ccbe9bf440e0
GPincheira <german1234esteban56@gmail.com>
2021-09-21 00:22:04 -0400

    Orden en form de agregar pregunta sugerida por estudiante

--- resources/views/livewire/agregar-pregsugerida.blade.php
+++ resources/views/livewire/agregar-pregsugerida.blade.php
@@ -16,78 +16,77 @@
 
     <div class="form-row">
       <div class="form-group col-md-7">
+        <div class="row row-bottom-margin">
+          <div class="form-group col-md-10">
+            <label for="preguntas"><b>Preguntas que desee agregar (requerido):</b></label>
+            <input wire:model="pregunta.0"  wire:keydown.enter="add({{$i}})" type="text" class="form-control" name="preguntas"></input>
+          </div>
+          <div class="form-group col-md-1">
+            <button class="btn text-white btn-info btn-sm" wire:click="add({{$i}})">Añadir</button>
+          </div>
+        </div>
+        @foreach($inputs as $key => $value)
           <div class="row row-bottom-margin">
-               <div class="form-group col-md-10">
-                  <b><label for="contexto">Seleccionar Contexto:</label></b>
-                      <br>
-                      <select wire:model="contexto" id="contexto">
-                        <option value="global">global</option>
-                        <option value="regular">regular</option>
-                        <option value="egresado">egresado</option>
-                      </select>
-                    </br>
-                </div>
             <div class="form-group col-md-10">
-              <label for="preguntas"><b>Preguntas que desee agregar (requerido):</b></label>
-              <input wire:model="pregunta.0"  wire:keydown.enter="add({{$i}})" type="text" class="form-control" name="preguntas"></input>
+              <input wire:model="pregunta.{{ $value }}"  wire:keydown.enter="add({{$i}})" type="text" class="form-control" name="preguntas"></input>
             </div>
             <div class="form-group col-md-1">
-              <button class="btn text-white btn-info btn-sm" wire:click="add({{$i}})">Añadir</button>
+                <button class="btn btn-danger btn-sm" wire:click="remove({{$key}})">X</button>
             </div>
           </div>
-          @foreach($inputs as $key => $value)
-            <div class="row row-bottom-margin">
-              <div class="form-group col-md-10">
-                <input wire:model="pregunta.{{ $value }}"  wire:keydown.enter="add({{$i}})" type="text" class="form-control" name="preguntas"></input>
-              </div>
-              <div class="form-group col-md-1">
-                  <button class="btn btn-danger btn-sm" wire:click="remove({{$key}})">X</button>
-              </div>
-            </div>
-          @endforeach
-          @error('pregunta.*')
-              <p class="text-danger">{{ $message }}</p>
-          @enderror
-    </div>
+        @endforeach
+        @error('pregunta.*')
+            <p class="text-danger">{{ $message }}</p>
+        @enderror
+      </div>
 
-    <div class="form-group col-md-5">
-      <div class="row">
-        <div class="form-group col-md-12">
-          <label for="vence"><b>Respuesta existente</b></label><br>
-          <input wire:model="respuesta_existente" type="checkbox" class="form-control">Añadir a respuesta existente
-          @if($respuesta_existente==null)
-            <label for="Respuesta"><b>Respuesta (requerido):</b></label>
-          <textarea wire:model="resp" type="text" rows="4" class="form-control" placeholder="Respuesta:"></textarea>
-          @else
-             <select wire:model="resp2">
+      <div class="form-group col-md-5">
+        <div class="row">
+          <div class="form-group col-md-12">
+            <label for="resp"><b>Respuesta (requerido):</b></label><br>
+            <input wire:model="respuesta_existente" type="checkbox" class="form-control">Añadir a respuesta existente <br>
+            @if($respuesta_existente==null)
+              <textarea wire:model="resp" name="resp" id="resp" type="text" rows="4" class="form-control" placeholder="Respuesta:"></textarea>
+            @else
+              <select wire:model="resp2">
                 @foreach($answers as $answer)
-                      @if($answer->nombre=="#!builtin_image-euONpC")
-                          <option value="#!builtin_image-euONpC">Mapa de Salas</option>
-                      @else
-                          <option value="{{$answer->id}}">{{$answer->nombre}}</option>
-                      @endif
+                  @if($answer->nombre=="#!builtin_image-euONpC")
+                    <option value="#!builtin_image-euONpC">Mapa de Salas</option>
+                  @else
+                    <option value="{{$answer->id}}">{{$answer->nombre}}</option>
+                  @endif
                 @endforeach
               </select>
-          @endif
+            @endif     
+          </div>
         </div>
+        <div class="row">
+          <div class="form-group col-md-12">
+            <label for="contexto"><b>Contexto:</b></label>              
+              <select wire:model="contexto" name="contexto">
+                <option value="global">Global: para todos los usuarios</option>
+                <option value="regular">Regular: Solo alumnos en estado regular</option>
+                <option value="egresado">Egresado: Solo alumnos egresados</option>
+              </select>
+          </div>
+        </div>
+        <div class="row">
+          <div class="form-group col-md-12">
+            <label for="vence"><b>Fecha de caducidad:</b></label><br>
+            <input wire:model="vence" type="checkbox" class="form-control"> Añadir fecha de caducidad
+          </div>
+        </div>  
+        <div class="row">
+          <div class="form-group col-md-12">
+            @if($vence == 1)
+              <input wire:model="fecha_caducacion" type="date" min="<?= date('Y-m-d'); ?>" class="form-control"> 
+            @else
+              <input wire:model="fecha_caducacion" type="date" class="form-control" disabled=""> 
+            @endif
+          </div>
+        </div>       
       </div>
-      <div class="row">
-        <div class="form-group col-md-12">
-          <label for="vence"><b>Fecha de caducidad:</b></label><br>
-          <input wire:model="vence" type="checkbox" class="form-control"> Añadir fecha de caducidad
-        </div>
-      </div>  
-      <div class="row">
-        <div class="form-group col-md-12">
-          @if($vence == 1)
-            <input wire:model="fecha_caducacion" type="date" class="form-control"> 
-          @else
-            <input wire:model="fecha_caducacion" type="date" class="form-control" disabled=""> 
-          @endif
-        </div>
-      </div>       
-    </div> 
-  </div>
+    </div>
     
     <div class="form-row justify-content-center">
           @if((strlen($resp)>0 && $respuesta_existente==0) && (($vence==1 && $fecha_caducacion!=null) || ($vence!=1)))
