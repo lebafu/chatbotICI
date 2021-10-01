0be2047abdc3b4a89d8ed20a13e7af44fe2545a6
GPincheira <german1234esteban56@gmail.com>
2021-10-01 00:58:17 -0400

    Migracion y modelo para categorias

--- database/migrations/2021_02_04_232426_answer.php
+++ database/migrations/2021_02_04_232426_answer.php
@@ -20,6 +20,7 @@ class Answer extends Migration
             $table->string('vence')->default('No');
             $table->date('fecha_caducacion')->nullable();
             //$table->foreignId('id_archivo')->references('id')->on('archivo_qna');
+            $table->foreignId('id_categoria')->references('id')->on('categorias');
             $table->string('archivo_qna');
             $table->integer('habilitada');
             $table->timestamps();
