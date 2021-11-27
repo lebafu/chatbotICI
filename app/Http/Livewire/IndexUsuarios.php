f9941465ef526c0f24cb3693dcaf08e36e79139f
lebafu <50503857+lebafu@users.noreply.github.com>
2021-11-26 21:29:21 -0300

    Creando usuario y añadiendo a usuario a team.
    Creando usuario y añadiendo a usuario a team.

--- app/Http/Livewire/IndexUsuarios.php
+++ app/Http/Livewire/IndexUsuarios.php
@@ -4,11 +4,14 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\User;
+use App\Models\Team;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\TestMail;
 use Illuminate\Support\Facades\Hash;
 use Livewire\WithPagination;
+use Carbon\Carbon;
+use DB;
 
 class IndexUsuarios extends Component
 {
@@ -51,6 +54,7 @@ class IndexUsuarios extends Component
         [
             'email.email' => 'Debes ingresar un email válido',
         ]);
+        $email=$this->email;
         $this->contraseña = $this->generate_string();
         $details=['title'=> 'Usted'.$this->name.'esta recibiendo la contraseña de la cuenta de Chatbot ICI',
                    'body'=> 'Su contraseña asignada es:'. $this->contraseña,
@@ -63,12 +67,23 @@ class IndexUsuarios extends Component
         ]);
         Mail::to($this->email)->send(new TestMail($details));
         $this->resetInput();
+
+        $user_id=DB::table('users')->max('id');
+        $id_actual_team=DB::table('teams')->max('id');
+        //dd($user_id,$id_actual_team,$this);
+         $carbon = new \Carbon\Carbon();
+        $fecha_actual = $carbon->now();
+        DB::table('teams')->insert(['user_id'=> $user_id,'name'=> 'Admin Team', 'personal_team'=>1,'created_at'=>$fecha_actual, 'updated_at'=>$fecha_actual]);
+
         session()->flash('message', 'Se ha registrado exitosamente este usuario. El nuevo usuario recibirá un email con su contraseña');
+
+        
     }
 
     public function delete($id)
     {
         User::find($id)->delete();
+        DB::table('teams')->where('user_id','=',$id)->delete();
         session()->flash('message', 'Usuario eliminado del sistema.');
     }
 }
